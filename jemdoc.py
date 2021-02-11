6be9dca6c21792433d7501700c1b6b26fa189449
dtahlyan <divyakant.tahlyan@gmail.com>
2021-02-10 23:05:41 -0600

    edit

--- jemdoc.py
+++ jemdoc.py
@@ -167,16 +167,16 @@ def standardconf():
   <body>
   
   [analytics]
-  <script type="text/javascript">
-  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-  </script>
-  <script type="text/javascript">
-  _uacct = "UA-108382219-1";
-  try {
-      var pageTracker = _gat._getTracker("|");
-      pageTracker._trackPageview();
-  } catch(err) {}</script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108382219-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108382219-1');
+</script>
+
   
   [menustart]
   <table summary="Table for page layout." id="tlayout">
